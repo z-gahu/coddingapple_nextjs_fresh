b1ffe2162d428cb808c2ffd62c66ea4ba64dd779
데스크탑 <z_gahu@naver.com>
2025-03-25 00:35:33 +0900

    navbar 추가

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
   let name = "park";
   return (
-    <div>
+    <main>
+      <div className="navbar">
+        <Link href="/">home</Link>
+        <Link href="/list">list페이지</Link>
+      </div>
       <h4 className="title" style={{ color: "red", fontSize: "20px" }}>
         애플후레시
       </h4>
       <p className="title-sub"> by dev {name}</p>
-    </div>
+    </main>
   );
 }

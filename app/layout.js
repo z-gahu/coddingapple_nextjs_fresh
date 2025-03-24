98b01475fffb1f1819ca413db4cad9a2ed151bd8
데스크탑 <z_gahu@naver.com>
2025-03-25 00:37:17 +0900

    페이지마다 반복적으로 보여줄때

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,6 +22,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <div className="navbar">
+          <Link href="/">home</Link>
+          <Link href="/list">list페이지</Link>
+        </div>
         {children}
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,13 +1,7 @@
-import Link from "next/link";
-
 export default function Home() {
   let name = "park";
   return (
     <main>
-      <div className="navbar">
-        <Link href="/">home</Link>
-        <Link href="/list">list페이지</Link>
-      </div>
       <h4 className="title" style={{ color: "red", fontSize: "20px" }}>
         애플후레시
       </h4>

a61ed8ab86dfcddbd2b1ef48e187b551cfea459b
데스크탑 <z_gahu@naver.com>
2025-03-28 16:53:21 +0900

    part1_5 map사용 적용

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,22 @@
 export default function List() {
+  let 상품 = ["Tomatoes", "Pasta", "Coconut"];
+  // let 어레이 = [2, 3, 4];
+  // let result = 어레이.map((a, i) => {
+  //   console.log(a, i);
+  //   return 10;
+  // });
+  // console.log(result);
+
   return (
     <div>
       <h2>Products</h2>
-      <div className="food">
-        <h4>상품명 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품명 $40</h4>
-      </div>
+      {상품.map(() => {
+        return (
+          <div className="food">
+            <h4>{상품[0]} $40</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }

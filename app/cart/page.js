98737c3aa9eb4834923ae8ca0c7d764a868fddf8
데스크탑 <z_gahu@naver.com>
2025-03-29 12:26:23 +0900

    장바구니 레이아웃 추가

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,7 +1,17 @@
 export default function Cart() {
   return (
     <div>
-      <h1>장바구니입니다.</h1>
+      <h4 className="title">Cart</h4>
+      <div className="cart-item">
+        <p>상품명</p>
+        <p>$40</p>
+        <p>1개</p>
+      </div>
+      <div className="cart-item">
+        <p>상품명</p>
+        <p>$40</p>
+        <p>1개</p>
+      </div>
     </div>
   );
 }

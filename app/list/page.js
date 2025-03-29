d854842f77877ceaa063b9ebd718e994a1af14fe
데스크탑 <z_gahu@naver.com>
2025-03-29 10:52:53 +0900

    img태그를 사용해 이미지 넣기

--- app/list/page.js
+++ app/list/page.js
@@ -1,3 +1,6 @@
+import Image from "next/image";
+// import 이미지 from "./food0.png";
+
 export default function List() {
   let 상품 = ["Tomatoes", "Pasta", "Coconut"];
   // let 어레이 = [2, 3, 4];
@@ -13,6 +16,7 @@ export default function List() {
       {상품.map((item, i) => {
         return (
           <div className="food" key={i}>
+            <img src={`/food${i}.png`} alt={item} className="food-img" />
             <h4>{item} $40</h4>
           </div>
         );

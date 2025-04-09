3d07abff822c8045156d886d5f5be88e18af4924
데스크탑 <z_gahu@naver.com>
2025-04-09 23:11:37 +0900

    좋아요버튼만들기_useState

--- app/list/page.js
+++ app/list/page.js
@@ -1,10 +1,14 @@
+"use client";
+
 import Image from "next/image";
 import 이미지0 from "/public/food0.png";
 import 이미지1 from "/public/food1.png";
 import 이미지2 from "/public/food2.png";
+import { useState } from "react";
 
 export default function List() {
   let 상품 = ["Tomatoes", "Pasta", "Coconut"];
+  let [수량, 수량변경] = useState(0);
   // let 어레이 = [2, 3, 4];
   // let result = 어레이.map((a, i) => {
   //   console.log(a, i);
@@ -29,6 +33,15 @@ export default function List() {
               // height={500}
             />
             <h4>{item} $40</h4>
+            <span> {수량} </span>
+            <button
+              onClick={() => {
+                // console.log(1);
+                수량변경(수량 + 1);
+              }}
+            >
+              +
+            </button>
           </div>
         );
       })}
